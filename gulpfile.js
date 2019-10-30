d6db144bc8fe3a1b113292d066783837efa0cf20
Anna Juszczak <anna.juszczak28@gmail.com>
2019-10-30 13:00:19 +0100

    Workshop Init

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest('css'))
+});
+gulp.task('dzien2', function (done) {
+    console.log("---------- popatrz do chroma----------");
+    done();
+});
+
+gulp.task('watch', function(){
+    gulp.watch('scss/*.scss', gulp.series("sass"));
+});
